334e2941044c293075aea33755efbc5f53e4bcec
wmuranak <w.m.2000.wm@gmail.com>
2020-09-27 06:48:30 +0000

    コメント修正

--- No12_task/No12_task_3.php
+++ No12_task/No12_task_3.php
@@ -2,7 +2,7 @@
 //フルーツを配列
 $fruits = array("apple","orange","banana","lemon","grape");
 
-//$fruitsから要素を一つずつ要素を取り出す
+//$fruitsから要素を一つずつを取り出す
 foreach($fruits as $fruit){
   echo "要素は".$fruit;
   echo "\n";
